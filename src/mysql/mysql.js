2bfbdb79754cd6a0f69772f671a38034f0ba6f74
Rohit <rahulkandwal707@gmail.com>
2023-08-19 17:32:53 +0530

    change file name app.js to index.js

--- src/mysql/mysql.js
+++ src/mysql/mysql.js
@@ -1,48 +1,48 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "Registration",
-});
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "Registration",
+// });
 
-module.exports.mysqlDatabase = async (query  ) => {
+// module.exports.mysqlDatabase = async (query  ) => {
   
   
-try{
-    const mysql_data = await  query_Mysql(query)
-    // console.log(' myswl data = ',mysql_data)
-    return mysql_data ;
-} 
-catch(e){
-    return e ;
-}
+// try{
+//     const mysql_data = await  query_Mysql(query)
+//     // console.log(' myswl data = ',mysql_data)
+//     return mysql_data ;
+// } 
+// catch(e){
+//     return e ;
+// }
    
-};
+// };
 
-// "select * from User_data ;"
-
-
-function query_Mysql ( query  ){
- return ( new Promise( (reject , resolve ) =>{
+// // "select * from User_data ;"
 
 
-        conn.query( query , (   error, result) => {
-            if (error) {
-                return reject(error); 
-            }
+// function query_Mysql ( query  ){
+//  return ( new Promise( (reject , resolve ) =>{
+
+
+//         conn.query( query , (   error, result) => {
+//             if (error) {
+//                 return reject(error); 
+//             }
           
-                resolve(JSON.parse(JSON.stringify(result)));
-                // conn.end((error) => {
-                //     if (error) {
-                //       console.error('Error closing MySQL connection:');
-                //       return;
-                //     }
+//                 resolve(JSON.parse(JSON.stringify(result)));
+//                 // conn.end((error) => {
+//                 //     if (error) {
+//                 //       console.error('Error closing MySQL connection:');
+//                 //       return;
+//                 //     }
                 
-                //     // console.log('MySQL connection closed.');
-                // });
-            }) ;
+//                 //     // console.log('MySQL connection closed.');
+//                 // });
+//             }) ;
 
-})  )
-}
+// })  )
+// }
